39c666ae2552917b148a47ceb3ae4d3cae8a1859
Arllain <arllain@gmail.com>
2021-09-17 22:17:00 -0300

    app login

---
+++ Login/Login/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.swift
+//  Login
+//
+//  Created by aluno on 17/09/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    var username: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let username = username else {
+            return
+        }
+        
+        welcomeLabel.text = "Welcome \(username)"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Login/Login/Segues.swift
@@ -0,0 +1,12 @@
+//
+//  Segues.swift
+//  Login
+//
+//  Created by aluno on 17/09/21.
+//
+
+import Foundation
+
+enum Segues: String {
+    case signedIn
+}
